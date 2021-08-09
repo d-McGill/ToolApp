47429893fae957b0e9c6f4536e0d0c9cce7de472
d-McGill <dmcgillz@icloud.com>
2021-08-09 17:48:04 +0100

    -c

--- application/views/addTool.php
+++ application/views/addTool.php
@@ -10,12 +10,14 @@
       </div>
     </div>
     <div class="col-sm">
-      <div class="form-floating">
-        <input type="text" class="form-control" id="floatingInput" name="status" required>
-        <label for="floatingInput">Status</label>
+        <label>Status</label>
+        <select class="form-select form-control" aria-label="Default select example" name="status" required>
+          <option selected disabled>Select One</option>
+          <option value="active">Active</option>
+          <option value="inactive">Inactive</option>
+        </select>
       </div>
     </div>
-  </div>
   <div class="row">
     <div class="col-sm">
       <div class="form-floating">
@@ -24,11 +26,21 @@
       </div>
     </div>
     <div class="col-sm">
-      <div class="form-floating">
-        <input type="text" class="form-control" id="floatingInput" name="cat" required>
-        <label for="floatingInput">Catagory</label>
+        <label>Catogry</label>
+        <select class="form-select form-control" aria-label="Default select example" name="cat" required>
+          <option selected disabled>Select One</option>
+          <option value="electric-tools">electric-tools</option>
+          <option value="CAT-and-Genny">electric-tools</option>
+          <option value="Hexatronic-Blowing-Kits">electric-tools</option>
+          <option value="Machinary">electric-tools</option>
+          <option value="Sundstrum">electric-tools</option>
+          <option value="breakers">electric-tools</option>
+          <option value="Compaction-Equiptment">electric-tools</option>
+          <option value="Stihll saw">Stihll saw</option>
+          <option value="Splicing-Equipment">electric-tools</option>
+          <option value="Havs-value-per-tool">electric-tools</option>
+        </select>
       </div>
-    </div>
   </div>
   <div class="row">
     <div class="col-sm">
